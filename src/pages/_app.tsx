bca6718567bdf378d6f94f703d6cca8196e55877
tmortx7 <99x7eo@gmail.com>
2023-02-24 14:54:41 -0700

    basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
-import { type AppType } from "next/app";
+import type { AppProps } from "next/app";
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import type { NextPage } from "next";
+import type { ReactElement, ReactNode } from "react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp = ({
+  Component,
+  pageProps: { ...pageProps },
+}: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const layout = getLayout(<Component {...pageProps} />);
+
+  return <div>{layout}</div>
 };
 
 export default api.withTRPC(MyApp);
